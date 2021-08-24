0f657ad08f3e24b82c5c877af5c4fda5f823728c
Carlos A. Cabrera <cacabrer@adobe.com>
2021-08-24 09:05:59 -0500

    fixed head

--- packages/pwa/pages/_app.tsx
+++ packages/pwa/pages/_app.tsx
@@ -34,7 +34,22 @@ const AppRoot = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>WKND Adventures</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <meta name="theme-color" content="#fdd835" />
+        <meta name="apple-mobile-web-app-title" content="WKND Adventures" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+        <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" crossOrigin="use-credentials" />
       </Head>
 
       <ApolloProvider client={apolloClient}>
