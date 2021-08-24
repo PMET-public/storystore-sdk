10db6deac1d9209e85b5410e6a86770a0ea2b294
Carlos A. Cabrera <cacabrer@adobe.com>
2021-08-24 14:58:41 -0500

    docs

--- packages/ui-kit/.storybook/main.ts
+++ packages/ui-kit/.storybook/main.ts
@@ -1,5 +1,6 @@
 export default {
   stories: [
+    './intro.stories.mdx',
     '../src/**/*.stories.mdx',
     //  '../src/**/*.stories.@(js|jsx|ts|tsx)' <- Only MDX stories for now. Gotta update babel and types buidls in packages.json if *.tsx enabled.
   ],
