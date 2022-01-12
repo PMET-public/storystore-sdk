025a7a3833c03f47dfe0f240a109c0ad57e33cef
Carlos A. Cabrera <cacabrer@adobe.com>
2022-01-12 16:32:36 -0600

    use resgrid instead

--- packages/pwa/pages/_page.tsx
+++ packages/pwa/pages/_page.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { AEMPage } from '@storystore/ui-kit/AEM'
+import { AEMResponsiveGrid } from '@storystore/ui-kit/AEM'
 import { Fragment } from 'react'
 
 const DynamicPage: NextPage<any> = () => {
@@ -9,7 +9,7 @@ const DynamicPage: NextPage<any> = () => {
 
   return (
     <Fragment key={asPath}>
-      <AEMPage pagePath={path} />
+      <AEMResponsiveGrid pagePath={path} itemPath="root/responsivegrid" />
     </Fragment>
   )
 }
