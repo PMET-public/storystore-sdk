97a49c3b551b626f7f61258130bb4ebb7ea5002d
Carlos A. Cabrera <cacabrer@adobe.com>
2022-02-01 09:17:37 -0600

    teaser

--- packages/pwa/components/AEMContentFragmentList/AEMContentFragmentList.tsx
+++ packages/pwa/components/AEMContentFragmentList/AEMContentFragmentList.tsx
@@ -7,7 +7,7 @@ import { Carousel, Tile, TileSkeleton, Heading } from '@storystore/ui-kit'
 const site = process.env.NEXT_PUBLIC_AEM_SITE
 
 // The sling:resourceType for which this Core Component is registered with in AEM
-const RESOURCE_TYPE = `${site}/components/contentfragmentlistadventures`
+const RESOURCE_TYPE = `${site}/components/contentfragmentlist`
 
 // Create an EditConfig to allow the AEM SPA Editor to properly render the component in the Editor's context
 const EditConfig = {
@@ -16,28 +16,24 @@ const EditConfig = {
   resourceType: RESOURCE_TYPE,
 }
 
-const ContentFragmentListAdventures = ({ ...props }) => {
-  console.log('ContentFragmentListAdventures', props)
-  const { id, items } = props
+const ContentFragmentList = ({ ...props }) => {
+  const { id, configTitleKey, configSubtitleKey, configImageKey, items } = props
 
   return (
     <Carousel id={id} show={{ sm: 1, md: 2, lg: 3 }} gap="sm" peak hideScrollBar>
       {EditConfig.isEmpty(props)
         ? [<TileSkeleton key={0} />, <TileSkeleton key={1} />, <TileSkeleton key={2} />, <TileSkeleton key={3} />]
         : items?.map(({ elements }, key) => {
-            const imageSrc = elements?.adventurePrimaryImage?.value
-            const title = elements?.adventureTitle?.value
-            const activity = elements?.adventureActivity?.value
-            const length = elements?.adventureTripLength?.value
-            const type = elements?.adventureType?.value
+            const imageSrc = elements?.[configImageKey]
+            const title = elements?.[configTitleKey]
+            const subtitle = elements?.[configSubtitleKey]
 
             return (
               <Tile
                 key={key}
                 image={<img src={imageSrc} alt={title} width={500} height={500} />}
                 heading={<Heading size="md">{title}</Heading>}
-                subheading={`${length} ${type}`}
-                tags={['#' + activity]}
+                subheading={subtitle}
               />
             )
           })}
@@ -46,7 +42,7 @@ const ContentFragmentListAdventures = ({ ...props }) => {
 }
 
 // MapTo allows the AEM SPA Editor JS SDK to dynamically render components added to SPA Editor Containers
-MapTo(RESOURCE_TYPE)(ContentFragmentListAdventures, EditConfig)
+MapTo(RESOURCE_TYPE)(ContentFragmentList, EditConfig)
 
 // withMappable allows the component to be hardcoded into the SPA; <AEMCarousel .../>
-export const AEMContentFragmentListAdventures = withMappable(ContentFragmentListAdventures, EditConfig)
+export const AEMContentFragmentList = withMappable(ContentFragmentList, EditConfig)
--- packages/pwa/components/AEMTeaser/AEMTeaser.tsx
+++ packages/pwa/components/AEMTeaser/AEMTeaser.tsx
@@ -1,5 +1,6 @@
 // Import the withMappable API provided bu the AEM SPA Editor JS SDK
-import { withMappable, MapTo } from '@adobe/aem-react-editable-components'
+import { TeaserV1IsEmptyFn, TeaserV1Model } from '@adobe/aem-core-components-react-base'
+import { withMappable, MapTo, MappedComponentProperties } from '@adobe/aem-react-editable-components'
 
 // Import the base  component
 import { Banner, Heading, Button, Link, Block, Text, Html } from '@storystore/ui-kit'
@@ -14,45 +15,72 @@ const RESOURCE_TYPE = `${site}/components/teaser`
 // Create an EditConfig to allow the AEM SPA Editor to properly render the component in the Editor's context
 const EditConfig = {
   emptyLabel: 'Teaser',
-  isEmpty: (props: any) => !props,
+  isEmpty: TeaserV1IsEmptyFn,
   resourceType: RESOURCE_TYPE,
 }
 
-const Teaser = ({ id, titleType, title, pretitle, linkURL, actions, description, imagePath }) => {
+const Teaser = ({ ...props }) => {
+  const { id, appliedCssClassNames = '', titleType, title, pretitle, linkURL, actions, description, imagePath } = props
+
+  /** Screen Color */
+  const variant = appliedCssClassNames.match(/teaser--variant-(hero)/)?.[1]
+
+  /** Screen Color */
+  const screen = appliedCssClassNames.match(/teaser--screen-(light|dark)/)?.[1]
+
+  /** Text Color  */
+  const _color = appliedCssClassNames.match(/teaser--color-(light|dark)/)?.[1] || 'dark'
+  const color = _color === 'light' ? '#fff' : '#222'
+
+  /** Alignment */
+  const align = appliedCssClassNames.match(/teaser--align-(left|center|right)/)?.[1] || 'center'
+
+  /** Vertical Alignment */
+  const vAlign = appliedCssClassNames.match(/teaser--valign-(top|middle|bottom)/)?.[1] || 'middle'
+
+  // Contained
+  const contained = !!appliedCssClassNames.match(/teaser--contained/)
+
   return (
     <Banner
       root={linkURL && !actions.length ? <Link href={linkURL} /> : undefined}
       id={id}
-      height="600px"
+      screen={screen}
+      align={align}
+      vAlign={vAlign}
+      backgroundColor="var(--color-skeleton)"
+      height={{ sm: variant === 'hero' ? '600px' : '260px', md: variant === 'hero' ? '800px' : '260px' }}
       backgroundImage={<img src={imagePath} alt="" loading="lazy" />}
+      contained={contained}
       heading={
-        <Block gap="xs">
+        <Block gap="xs" style={{ color }}>
           {pretitle && (
             <Heading root={<div />} size="sm">
               {pretitle}
             </Heading>
           )}
 
-          <Heading root={titleType && createElement(titleType)} size="md">
+          <Heading root={titleType && createElement(titleType)} size={{ sm: '2xl', md: '3xl', lg: '5xl' }}>
             {title}
           </Heading>
 
-          <Text root={<Html htmlString={description} />} size="sm" />
-
-          {actions && (
-            <Block
-              columns={{ sm: 'max-content', md: 'repeat(auto-fit, minmax(100px, 1fr))' }}
-              align="center"
-              gap="sm"
-              contained
-            >
-              {actions.map(({ id, title, url }) => (
-                <Button key={id} root={<Link href={url} />} variant="cta">
-                  {title}
-                </Button>
-              ))}
-            </Block>
-          )}
+          {description && <Text root={<Html htmlString={description} />} size="sm" />}
+        </Block>
+      }
+      button={
+        <Block gap="sm" style={{ color }}>
+          {actions.map(({ title, url }, key) => (
+            <div key={key}>
+              <Button
+                root={<Link href={url} />}
+                variant="cta"
+                transparent
+                style={{ ['--button-text' as string]: 'inherit' }}
+              >
+                {title}
+              </Button>
+            </div>
+          ))}
         </Block>
       }
     />
@@ -60,7 +88,7 @@ const Teaser = ({ id, titleType, title, pretitle, linkURL, actions, description,
 }
 
 // MapTo allows the AEM SPA Editor JS SDK to dynamically render components added to SPA Editor Containers
-MapTo<any>(RESOURCE_TYPE)(Teaser, EditConfig)
+MapTo<MappedComponentProperties & TeaserV1Model>(RESOURCE_TYPE)(Teaser, EditConfig)
 
 // withMappable allows the component to be hardcoded into the SPA; <AEMTeaser .../>
-export const AEMTeaser = withMappable(Teaser, EditConfig)
+export const AEMTeaser = withMappable<MappedComponentProperties & TeaserV1Model>(Teaser, EditConfig)
