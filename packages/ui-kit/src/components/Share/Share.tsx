d937c5792802eabc6536c75cd26ca1440dd52880
Carlos A. Cabrera <cacabrer@adobe.com>
2022-02-09 17:13:51 -0600

    Share

---
+++ packages/ui-kit/src/components/Share/Share.tsx
@@ -0,0 +1,53 @@
+import { FunctionComponent, HTMLAttributes, ReactElement, useCallback, useEffect, useState } from 'react'
+import { classes, merge } from '../../lib'
+
+// Styles
+import styles from './Share.module.css'
+
+// Icons
+import ShareIcon from 'remixicon-react/ShareForwardFillIcon'
+import { Button } from '..'
+
+export type ShareProps = HTMLAttributes<HTMLElement> & {
+  root?: ReactElement
+  title?: string
+  text?: string
+  url?: string
+}
+
+export const Share: FunctionComponent<ShareProps> = ({ root = <div />, title, text, url, className, ...props }) => {
+  const [disabled, setDisabled] = useState(false)
+
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setDisabled(!navigator?.canShare({ title, text, url }))
+  }, [title, text, url])
+
+  const handleShare = useCallback(async () => {
+    try {
+      setError(false)
+
+      await navigator.share({
+        title,
+        text,
+        url,
+      })
+    } catch (err) {
+      setError(true)
+    }
+  }, [title, text, url])
+
+  return (
+    <root.type {...merge(props, root.props)} className={classes([styles.root, root.props.className, className])}>
+      <Button
+        transparent
+        icon={<ShareIcon />}
+        aria-label="Share"
+        disabled={disabled}
+        onClick={handleShare}
+        className={classes([[styles.error, error]])}
+      />
+    </root.type>
+  )
+}
